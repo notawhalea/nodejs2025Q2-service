7891e25ad1dde64ca6b5e9abe31b39f702484dad
Nikita <nickpelmog2003@gmail.com>
2025-11-30 20:10:04 +0300

    fix: update .env file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 4000);
+  const port = process.env.PORT || 4000;
+  console.log(`Aplication is listening on http://localhost:${port}/`);
+  await app.listen(port);
 }
 bootstrap();
